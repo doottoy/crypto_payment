8c6b469ac0373479e8b1d32226a63295b00a2a2a
Anton Yevchenko <antonyevchenko@MacBook-Pro-Anton-Evchenko.local>
2024-10-21 12:53:20 +0300

    [AY-0004] add polling block tracker

--- src/services/payout.service.ts
+++ src/services/payout.service.ts
@@ -1,6 +1,7 @@
 /* External dependencies */
 import Web3 from 'web3';
 import HDWalletProvider from '@truffle/hdwallet-provider';
+import { PollingBlockTracker } from 'eth-block-tracker';
 
 /* Internal dependencies */
 import { modules } from '../utils/modules';
@@ -15,8 +16,10 @@ import { Const } from '../constants/const';
 export class PayoutService {
     private provider!: HDWalletProvider;
     private web3!: Web3;
+    private rpcUrl!: string;
     private senderAddress!: string;
     private decimalsCache: Record<string, number> = {};
+    private blockTracker!: PollingBlockTracker;
 
     /**
      * Constructs a new instance of PayoutService.
@@ -30,10 +33,10 @@ export class PayoutService {
      * Should be called before sending transactions.
     */
     async init() {
-        const rpcUrl = await modules.getRpcUrl(this.payway);
+        this.rpcUrl = await modules.getRpcUrl(this.payway);
         this.provider = new HDWalletProvider({
             privateKeys: [this.privateKey],
-            providerOrUrl: rpcUrl
+            providerOrUrl: this.rpcUrl
         });
         this.web3 = new Web3(this.provider as any);
         this.senderAddress = this.provider.getAddress(0);
@@ -84,6 +87,20 @@ export class PayoutService {
     }
 
     /**
+     * Gets a dynamic multiplier for gas price based on network congestion.
+     * @returns The multiplier to adjust the gas price.
+     */
+    private async getDynamicMultiplier(): Promise<number> {
+        try {
+            const gasPrice = Number(await this.web3.eth.getGasPrice());
+            return gasPrice < 20e9 ? 1.2 : gasPrice < 50e9 ? 1.5 : 2.0;
+        } catch (error) {
+            console.error(`Error fetching dynamic multiplier: ${error}`);
+            return 1.5;
+        }
+    }
+
+    /**
      * Sends a transaction to the specified payee address with the given amount.
      * Supports both native token transfers and ERC-20 token transfers.
      *
@@ -92,18 +109,36 @@ export class PayoutService {
      * @param contract - The contract address of the token (if applicable).
      * @param currency - The currency being used (e.g., ETH, BNB).
      * @returns The transaction hash of the successful transaction.
-    */
+     */
     async sendTransaction(payee_address: string, amount: string, contract: string, currency: string): Promise<string> {
         try {
-            // Get current nonce and gas price
+            // Initialize block tracker
+            this.blockTracker = new PollingBlockTracker({
+                provider: this.web3.currentProvider as any,
+                pollingInterval: 1000
+            });
+
+            // Tracking new blocks
+            this.blockTracker.on('latest', (blockNumber: string) => {
+                console.log(`New block detected from provider ${this.rpcUrl}: ${blockNumber}`);
+            });
+
+            this.blockTracker.on('error', (error: Error) => {
+                console.error(`Block tracker error from provider ${this.rpcUrl}: ${error.message}`);
+            });
+
+            // Get current nonce and gas price, and increase gas price for faster inclusion
             const [actualNonce, gasPrice] = await Promise.all([
                 this.web3.eth.getTransactionCount(this.senderAddress),
                 this.web3.eth.getGasPrice()
             ]);
 
+            // Increase gas price by the specified multiplier to speed up the transaction
+            const increasedGasPrice = Math.ceil(Number(gasPrice) * await this.getDynamicMultiplier()).toString();
+
             let tx: any = {
                 from: this.senderAddress,
-                gasPrice,
+                gasPrice: increasedGasPrice,
                 nonce: actualNonce,
                 timeout: 300000
             };
@@ -123,7 +158,7 @@ export class PayoutService {
 
             // Estimate and set gas limit with buffer
             const estimatedGas = await this.web3.eth.estimateGas(tx);
-            tx.gas = Math.min(estimatedGas + 10000, Const.MULTI_SEND_GAS_LIMIT);
+            tx.gas = Math.min(estimatedGas + 100000);
 
             // Signed and send signed transaction
             const signedTx = await this.web3.eth.accounts.signTransaction(tx, this.privateKey);
@@ -133,10 +168,17 @@ export class PayoutService {
             console.log(notifierMessage.formatSuccessEVMTransaction(this.payway, currency, transaction));
             await modules.sendMessageToTelegram(notifierMessage.formatSuccessEVMTransaction(this.payway, currency, transaction));
 
+            // Stop tracking new blocks
+            this.blockTracker.removeAllListeners();
+
             return transaction.transactionHash;
         } catch (error) {
             // Notify about the error
             await modules.sendMessageToTelegram(notifierMessage.formatErrorEVM(this.payway, currency, error));
+
+            // Stop tracking new blocks
+            this.blockTracker.removeAllListeners();
+
             throw error;
         }
     }
